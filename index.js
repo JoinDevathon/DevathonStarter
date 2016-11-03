f5902290d94280d8b2597b00efd4696cc1771103
Paul Sauve <paul@burngames.net>
2016-11-03 14:39:34 -0500

    Add start/end

--- index.js
+++ index.js
@@ -14,30 +14,30 @@ const pool = mysql.createPool({
 function createRepositories(usernames) {
     // create a repository for each, then run a bash script to push to it
     Promise.all(usernames.map(username => fetch(`https://DevathonBot:${P_TOKEN}@api.github.com/orgs/JoinDevathon/repos`, {
-        method: 'POST',
-        headers: {
-            'User-Agent': 'DevathonStarter',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name: `${username}-2016`,
-            description: 'This is the repository for your Devathon Project.',
-            homepage: 'https://devathon.org',
-            private: true,
-        })
-    })
-        .then(res => {
-            return fetch(`https://DevathonBot:${P_TOKEN}@api.github.com/repos/JoinDevathon/${username}-2016/collaborators/${username}`, {
-                method: 'PUT',
-                headers: {
-                    'User-Agent': 'DevathonStarter',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    permission: 'push'
-                })
+            method: 'POST',
+            headers: {
+                'User-Agent': 'DevathonStarter',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: `${username}-2016`,
+                description: 'This is the repository for your Devathon Project.',
+                homepage: 'https://devathon.org',
+                private: true,
             })
         })
+            .then(res => {
+                return fetch(`https://DevathonBot:${P_TOKEN}@api.github.com/repos/JoinDevathon/${username}-2016/collaborators/${username}`, {
+                    method: 'PUT',
+                    headers: {
+                        'User-Agent': 'DevathonStarter',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        permission: 'push'
+                    })
+                })
+            })
     ))
         .then(() => console.log('Made repos for', usernames))
         .then(() => {
@@ -49,11 +49,43 @@ function createRepositories(usernames) {
         .catch(err => console.error('Failed to make repos for', usernames, err));
 }
 
-pool.query('SELECT `username` FROM `users`', function (err, rows) {
-    if (err) {
-        throw err;
-    }
-    const usernames = rows.map(({username}) => username);
-    createRepositories(usernames);
-    pool.end();
-});
+function start() {
+    pool.query('SELECT `username` FROM `users`', function (err, rows) {
+        if (err) {
+            throw err;
+        }
+        const usernames = rows.map(({username}) => username);
+        createRepositories(usernames);
+    });
+}
+
+function closeRepositories(usernames) {
+    Promise.all(usernames.map(username => fetch(`https://DevathonBot:${P_TOKEN}@api.github.com/repos/JoinDevathon/${username}-2016/collaborators/${username}`, {
+        method: 'DELETE',
+        headers: {
+            'User-Agent': 'DevathonStarter',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+        })
+    })))
+        .catch(err => console.error('Failed to close repositories', err));
+}
+
+function end() {
+    pool.query('SELECT `username` FROM `users`', function (err, rows) {
+        if (err) {
+            throw err;
+        }
+        const usernames = rows.map(({username}) => username);
+        closeRepositories(usernames);
+    });
+}
+
+const START = new Date(1478350800 * 1000);
+const END = new Date(1478440800 * 1000);
+const NOW = Date.now();
+
+setTimeout(start, START - NOW);
+setTimeout(end, END - NOW);
+
