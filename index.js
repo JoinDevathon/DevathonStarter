1c317cdab991c64aab210f5f3458ca0f259fe312
Paul Sauve <paul@burngames.net>
2016-11-05 08:12:53 -0500

    DO STUFF

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const mysql = require('mysql');
 const fetch = require('node-fetch');
+const {createTransport} = require('nodemailer');
 const {execFileSync} = require('child_process');
 const P_TOKEN = 'af36284f3ebdcbb56de18a24cae7a85b7a1cdd87';
+
+const transporter = createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'joindevathon@gmail.com',
+        pass: 'a2096d45-a57e-4f77-8422-877ffc234e46'
+    }
+});
 
 const pool = mysql.createPool({
     connectionLimit: 2,
@@ -49,15 +58,67 @@ function createRepositories(usernames) {
         .catch(err => console.error('Failed to make repos for', usernames, err));
 }
 
+function unprivateAndAdd(usernames) {
+    Promise.all(usernames.map(username => fetch(`https://DevathonBot:${P_TOKEN}@api.github.com/repos/JoinDevathon/${username}-2016/collaborators/${username}`, {
+        method: 'PUT',
+        headers: {
+            'User-Agent': 'DevathonStarter',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            permission: 'push'
+        })
+    })))
+        .then(Promise.all(usernames.map(username => fetch(`https://DevathonBot:${P_TOKEN}@api.github.com/repos/JoinDevathon/${username}-2016`, {
+            method: 'PATCH',
+            headers: {
+                'User-Agent': 'DevathonStarter',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: `${username}-2016`,
+                private: false
+            })
+        }))));
+}
+
+function sendEmails(users) {
+    users.forEach(({email, username}) => {
+        transporter.sendMail({
+            from: '"Devathon" <joindevathon@gmail.com>',
+            to: email,
+            subject: 'Devathon Has Started!',
+            text: `Devathon has just started. Your project is located at https://github.com/JoinDevathon/${username}-2016`,
+            html: `Devathon has just started. Your project is located at https://github.com/JoinDevathon/${username}-2016`,
+        }, (err, info) => {
+            if (err) {
+                console.error('Failed to send email to', email, username, err);
+            }
+        })
+    })
+}
+
 function start() {
-    pool.query('SELECT `username` FROM `users`', function (err, rows) {
+    pool.query('SELECT `username`,`email` FROM `users`', function (err, rows) {
         if (err) {
             throw err;
         }
         const usernames = rows.map(({username}) => username);
         createRepositories(usernames);
+        sendEmails(rows);
     });
 }
+
+function doStuff() {
+    pool.query('SELECT `username`,`email` FROM `users`', function (err, rows) {
+        if (err) {
+            throw err;
+        }
+        const usernames = rows.map(({username}) => username);
+        unprivateAndAdd(usernames);
+    });
+}
+
 
 function closeRepositories(usernames) {
     Promise.all(usernames.map(username => fetch(`https://DevathonBot:${P_TOKEN}@api.github.com/repos/JoinDevathon/${username}-2016/collaborators/${username}`, {
@@ -66,8 +127,7 @@ function closeRepositories(usernames) {
             'User-Agent': 'DevathonStarter',
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-        })
+        body: JSON.stringify({})
     })))
         .catch(err => console.error('Failed to close repositories', err));
 }
@@ -86,6 +146,7 @@ const START = new Date(1478350800 * 1000);
 const END = new Date(1478440800 * 1000);
 const NOW = Date.now();
 
-setTimeout(start, START - NOW);
+// setTimeout(start, START - NOW);
 setTimeout(end, END - NOW);
+doStuff();
 
